dab3851d90fd7f8ceca4e760fca383ef360fc0b1
Hebrom Souza <hebromsouza@gmail.com>
2026-05-20 22:51:06 -0300

    [QA Pipeline] adiciona elements/InventoryItemElements.ts

---
+++ elements/InventoryItemElements.ts
@@ -0,0 +1,8 @@
+export class InventoryItemElements {
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs6Backtoproducts = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs6back-to-products"]'
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs6 = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs6"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly img = '[data-test="img"]'
+  static readonly 6 = '[data-test="6"]'
+}
