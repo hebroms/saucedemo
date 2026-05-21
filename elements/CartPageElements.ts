5dfcf1da810d7d931b4e5fee09eaa009bc481ac3
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:29 -0300

    [QA Pipeline] adiciona elements/CartPageElements.ts

---
+++ elements/CartPageElements.ts
@@ -0,0 +1,9 @@
+// Locators for CartPage
+
+export const CART_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+  cartTitle: 'your-cart',
+}
