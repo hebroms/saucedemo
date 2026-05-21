8b5bfa348d3ef9a6a2e234d0a98aa469b0891df1
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:31 -0300

    [QA Pipeline] adiciona elements/CheckoutStepOnePageElements.ts

---
+++ elements/CheckoutStepOnePageElements.ts
@@ -0,0 +1,9 @@
+// Locators for CheckoutStepOnePage
+
+export const CHECKOUT_STEP_ONE_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+  checkoutTitle: 'checkout: your information',
+}
