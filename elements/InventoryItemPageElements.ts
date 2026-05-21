287c70a2aafaba894738cceb879107570abb03a7
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:38 -0300

    [QA Pipeline] adiciona elements/InventoryItemPageElements.ts

---
+++ elements/InventoryItemPageElements.ts
@@ -0,0 +1,9 @@
+// Locators for InventoryItemPage
+
+export const INVENTORY_ITEM_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+  itemNumber: '6', // Example locator for the item number
+}
