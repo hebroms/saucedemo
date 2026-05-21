a9adec83ef1d1552aaa78c4fa57df126006881c2
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:36 -0300

    [QA Pipeline] adiciona elements/CheckoutCompletePageElements.ts

---
+++ elements/CheckoutCompletePageElements.ts
@@ -0,0 +1,9 @@
+// Locators for CheckoutCompletePage
+
+export const CHECKOUT_COMPLETE_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+  completeTitle: 'checkout: complete!'
+}
