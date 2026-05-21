e3fd4947d85bb9c485ade0bd9f6c80cd08b7e1a5
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:24 -0300

    [QA Pipeline] adiciona elements/HomePageElements.ts

---
+++ elements/HomePageElements.ts
@@ -0,0 +1,8 @@
+// Locators for HomePage
+
+export const HOME_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+}
