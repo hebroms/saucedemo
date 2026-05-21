adf4bea6129120c6dd4b519b7354bc2fd3d59e7a
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:48 -0300

    [QA Pipeline] atualiza elements/CartElements.ts

--- elements/CartElements.ts
+++ elements/CartElements.ts
@@ -1,8 +1,3 @@
 export class CartElements {
-  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabsYourCart = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labsyour-cart"]'
-  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs"]'
-  static readonly openMenu = '[data-test="open-menu"]'
-  static readonly openMenu = '[data-test="open-menu"]'
-  static readonly img = '[data-test="img"]'
-  static readonly a = '[data-test="a"]'
+  static readonly root = 'body'
 }
