f9163f1169fb7730805bbfb8887a6ea956089313
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:45 -0300

    [QA Pipeline] adiciona elements/CheckoutStepOneElements.ts

---
+++ elements/CheckoutStepOneElements.ts
@@ -0,0 +1,8 @@
+export class CheckoutStepOneElements {
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabsCheckoutYourInformation = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labscheckout:-your-information"]'
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly img = '[data-test="img"]'
+  static readonly a = '[data-test="a"]'
+}
