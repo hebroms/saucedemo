a1b3df0a338300a55a23e7a6541f67fad262f5e1
Hebrom Souza <hebromsouza@gmail.com>
2026-05-20 22:51:01 -0300

    [QA Pipeline] adiciona elements/CheckoutCompleteElements.ts

---
+++ elements/CheckoutCompleteElements.ts
@@ -0,0 +1,8 @@
+export class CheckoutCompleteElements {
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabsCheckoutComplete = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labscheckout:-complete!"]'
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly img = '[data-test="img"]'
+  static readonly a = '[data-test="a"]'
+}
