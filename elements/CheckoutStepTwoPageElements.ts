650e25604558464083f9fdf612c1858fa497abec
Hebrom Souza <hebromsouza@gmail.com>
2026-05-21 12:30:33 -0300

    [QA Pipeline] adiciona elements/CheckoutStepTwoPageElements.ts

---
+++ elements/CheckoutStepTwoPageElements.ts
@@ -0,0 +1,9 @@
+// Locators for CheckoutStepTwoPage
+
+export const CHECKOUT_STEP_TWO_LOCATORS: ElementLocators = {
+  menuButton: 'menu-button',
+  itemsMenu: 'all-items',
+  logoutButton: 'logout',
+  appName: 'sauce-demo',
+  checkoutTitle: 'checkout: overview',
+}
