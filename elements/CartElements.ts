fbba408d4cf710bcd7f3ac0171d3e5fad2bc802c
Hebrom Souza <hebromsouza@gmail.com>
2026-06-02 11:50:53 -0300

    [QA Pipeline] adiciona elements/CartElements.ts

---
+++ elements/CartElements.ts
@@ -0,0 +1,8 @@
+export class CartElements {
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabsYourCart = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labsyour-cart"]'
+  static readonly openMenuAllItemsAboutLogoutResetAppStateCloseMenuSwagLabs = '[data-test="open-menuall-itemsaboutlogoutreset-app-stateclose-menuswag-labs"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly openMenu = '[data-test="open-menu"]'
+  static readonly img = '[data-test="img"]'
+  static readonly a = '[data-test="a"]'
+}
