b97a573314fe5b571fbbf498a86df8919c8a7f7c
Hebrom Souza <hebromsouza@gmail.com>
2026-07-11 17:28:45 -0300

    [QA Pipeline] adiciona elements/CartPageElements.ts

---
+++ elements/CartPageElements.ts
@@ -0,0 +1,11 @@
+// Locators específicos para a CartPage
+import { Locator } from '@playwright/test';
+
+/**
+ * Seletores específicos para a CartPage.
+ */
+export class CartPageElements {
+  // Menu e Navegação
+  inventoryMenu: Locator;
+  cartItems: Locator;
+}
