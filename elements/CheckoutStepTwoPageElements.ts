786611f8b7bbb0cd92b447be62d7a7eb07543297
Hebrom Souza <hebromsouza@gmail.com>
2026-07-11 17:28:49 -0300

    [QA Pipeline] adiciona elements/CheckoutStepTwoPageElements.ts

---
+++ elements/CheckoutStepTwoPageElements.ts
@@ -0,0 +1,10 @@
+// Locators específicos para o CheckoutStepTwoPage
+import { Locator } from '@playwright/test';
+
+/**
+ * Seletores específicos para o CheckoutStepTwoPage.
+ */
+export class CheckoutStepTwoPageElements {
+  // Menu e Navegação
+  inventoryMenu: Locator;
+}
