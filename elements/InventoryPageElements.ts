b7ea1225b79286a0cce317530322cca44c1ef83d
Hebrom Souza <hebromsouza@gmail.com>
2026-07-11 17:28:43 -0300

    [QA Pipeline] adiciona elements/InventoryPageElements.ts

---
+++ elements/InventoryPageElements.ts
@@ -0,0 +1,10 @@
+// Locators específicos para a InventoryPage
+import { Locator } from '@playwright/test';
+
+/**
+ * Seletores específicos para a InventoryPage.
+ */
+export class InventoryPageElements {
+  // Menu e Navegação
+  inventoryMenu: Locator;
+}
