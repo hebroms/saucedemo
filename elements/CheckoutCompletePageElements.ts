c1c608825a1f934aa3c257b4fbb79ccfb73c83b7
Hebrom Souza <hebromsouza@gmail.com>
2026-07-11 17:28:56 -0300

    [QA Pipeline] adiciona elements/CheckoutCompletePageElements.ts

---
+++ elements/CheckoutCompletePageElements.ts
@@ -0,0 +1,10 @@
+// Locators específicos para o CheckoutCompletePage
+import { Locator } from '@playwright/test';
+
+/**
+ * Seletores específicos para o CheckoutCompletePage.
+ */
+export class CheckoutCompletePageElements {
+  // Menu e Navegação
+  inventoryMenu: Locator;
+}
