65d3aa357d238d89d846c1436d371a34e8acd125
Hebrom Souza <hebromsouza@gmail.com>
2026-07-11 17:28:47 -0300

    [QA Pipeline] adiciona elements/CheckoutStepOnePageElements.ts

---
+++ elements/CheckoutStepOnePageElements.ts
@@ -0,0 +1,10 @@
+// Locators específicos para o CheckoutStepOnePage
+import { Locator } from '@playwright/test';
+
+/**
+ * Seletores específicos para o CheckoutStepOnePage.
+ */
+export class CheckoutStepOnePageElements {
+  // Menu e Navegação
+  inventoryMenu: Locator;
+}
