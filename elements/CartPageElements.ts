fec9438129e65922e67512593a758b319be0d5fc
Hebrom Souza <hebromsouza@gmail.com>
2026-08-14 16:48:52 -0300

    [QA Pipeline] adiciona elements/CartPageElements.ts

---
+++ elements/CartPageElements.ts
@@ -0,0 +1,11 @@
+// Locators for CartPage
+export const CartPageElements = {
+  menu: 'a[data-testid="menu-item"]',
+  allItems: 'a[data-testid="all-items"]',
+  about: 'a[data-testid="about"]',
+  logout: 'a[data-testid="logout_button"]',
+  resetAppState: 'button[data-testid="reset_app_state"]',
+  closeMenu: 'button[data-testid="close_menu"]',
+  swagLabs: 'a[data-testid="swag_labs"]',
+  cartItems: 'a[data-testid="cart_items"]',
+}
