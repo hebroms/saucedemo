b2fd2f2358f145c21226ad4e9cfc3518be596127
Hebrom Souza <hebromsouza@gmail.com>
2026-08-14 16:48:38 -0300

    [QA Pipeline] adiciona playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from '@playwright/test'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 4 : 2,
+  reporter: [['html'], ['list']],
+  use: {
+    baseURL: process.env.BASE_URL || 'https://www.saucedemo.com/',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ],
+})
