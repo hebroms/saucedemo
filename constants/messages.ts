9024105ef93544aa0666d1c25ab9e3da849e8722
Hebrom Souza <hebromsouza@gmail.com>
2026-08-14 16:48:42 -0300

    [QA Pipeline] adiciona constants/messages.ts

---
+++ constants/messages.ts
@@ -0,0 +1,8 @@
+export const MESSAGES = {
+  successLogin: "Login successful!",
+  errorInvalidCredentials: "Epic sadface: Sorry, it looks like you don't have permission to access this page.",
+  errorInvalidEmailFormat: "Validation Error: Invalid email format. Please use a valid email address.",
+  errorRateLimit: "Too many failed attempts. Access temporarily blocked.",
+  successCart: "Your Cart is ready for checkout.",
+  successComplete: "Checkout complete!",
+};
