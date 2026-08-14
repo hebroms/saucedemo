9c6903afaf6d3fd4b91c7d01b2d452d6f955b266
Hebrom Souza <hebromsouza@gmail.com>
2026-08-14 16:49:01 -0300

    [QA Pipeline] adiciona elements/HomeElements.ts

---
+++ elements/HomeElements.ts
@@ -0,0 +1,8 @@
+export class HomeElements {
+  static readonly acceptedusernamesarestandarduserlockedoutuserproblemuserperformanceglitchusererroruservisu = '[data-test="accepted-usernames-are:standard_userlocked_out_userproblem_userperformance_glitch_usererror_uservisu"]'
+  static readonly username = '[data-test="username"]'
+  static readonly password = '[data-test="password"]'
+  static readonly loginbutton = '[data-test="login-button"]'
+  static readonly acceptedusernamesarestandarduserlockedoutuserproblemuserperformanceglitchusererroruservisu = '[data-test="accepted-usernames-are:standard_userlocked_out_userproblem_userperformance_glitch_usererror_uservisu"]'
+  static readonly acceptedusernamesarestandarduserlockedoutuserproblemuserperformanceglitchusererroruservisu = '[data-test="accepted-usernames-are:standard_userlocked_out_userproblem_userperformance_glitch_usererror_uservisu"]'
+}
