694548a38de92d2019ad9c76be7daae8446aa440
Hebrom Souza <hebromsouza@gmail.com>
2026-08-14 16:48:50 -0300

    [QA Pipeline] adiciona elements/InventoryItemPageElements.ts

---
+++ elements/InventoryItemPageElements.ts
@@ -0,0 +1,11 @@
+// Locators for InventoryItemPage (e.g., item details)
+export const InventoryItemPageElements = {
+  menu: 'a[data-testid="menu-item"]',
+  allItems: 'a[data-testid="all-items"]',
+  about: 'a[data-testid="about"]',
+  logout: 'a[data-testid="logout_button"]',
+  resetAppState: 'button[data-testid="reset_app_state"]',
+  closeMenu: 'button[data-testid="close_menu"]',
+  swagLabs: 'a[data-testid="swag_labs"]',
+  itemBackToProducts: 'a[data-testid="back_to_products"]',
+}
